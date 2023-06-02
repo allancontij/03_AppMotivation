44fd4a57b8fb19bcdd105acd47ee6cf131b684b2
Allan Conti Junior <58399481+allancontij@users.noreply.github.com>
2023-06-02 12:53:16 -0300

    First Commit.

---
+++ app/src/main/java/devandroid/allan/appmotivation/MainActivity.kt
@@ -0,0 +1,37 @@
+package devandroid.allan.appmotivation
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.View
+import devandroid.allan.appmotivation.databinding.ActivityMainBinding
+
+class MainActivity : AppCompatActivity(), View.OnClickListener {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        supportActionBar?.hide()
+
+        handleUserName()
+
+        binding.buttonNewPhrase.setOnClickListener(this)
+
+    }
+
+    override fun onClick(view: View) {
+        if (view.id == R.id.button_new_phrase) {
+            var s = ""
+        }
+    }
+
+    private fun handleUserName() {
+        val name = SecurityPreferences(this).getString("USER_NAME")
+        binding.textUserName.text = "Olá, $name!"
+    }
+
+}
