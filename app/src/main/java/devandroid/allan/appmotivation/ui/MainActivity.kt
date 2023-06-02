e3187ee6598f7fa38e36f4b262ab514071b26f15
Allan Conti Junior <58399481+allancontij@users.noreply.github.com>
2023-06-02 14:16:30 -0300

    Alterations in menu, get id of menu state.

--- app/src/main/java/devandroid/allan/appmotivation/ui/MainActivity.kt
+++ app/src/main/java/devandroid/allan/appmotivation/ui/MainActivity.kt
@@ -3,6 +3,7 @@ package devandroid.allan.appmotivation.ui
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
+import androidx.core.content.ContextCompat
 import devandroid.allan.appmotivation.infra.MotivationConstants
 import devandroid.allan.appmotivation.R
 import devandroid.allan.appmotivation.infra.SecurityPreferences
@@ -11,6 +12,7 @@ import devandroid.allan.appmotivation.databinding.ActivityMainBinding
 class MainActivity : AppCompatActivity(), View.OnClickListener {
 
     private lateinit var binding: ActivityMainBinding
+    private var categoryId = 1
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -23,12 +25,39 @@ class MainActivity : AppCompatActivity(), View.OnClickListener {
         handleUserName()
 
         binding.buttonNewPhrase.setOnClickListener(this)
+        binding.imageAll.setOnClickListener(this)
+        binding.imageHappy.setOnClickListener(this)
+        binding.imageSunny.setOnClickListener(this)
 
     }
 
     override fun onClick(view: View) {
         if (view.id == R.id.button_new_phrase) {
             var s = ""
+        } else if (view.id in listOf(R.id.image_all, R.id.image_happy, R.id.image_sunny)) {
+            handlerFilter(view.id)
+        }
+    }
+
+    private fun handlerFilter(id: Int) {
+
+        binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.black))
+        binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.black))
+
+        when (id) {
+            R.id.image_all -> {
+                binding.imageAll.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.ALL
+            }
+            R.id.image_happy -> {
+                binding.imageHappy.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.HAPPY
+            }
+            R.id.image_sunny -> {
+                binding.imageSunny.setColorFilter(ContextCompat.getColor(this, R.color.white))
+                categoryId = MotivationConstants.FILTER.SUNNY
+            }
         }
     }
 
